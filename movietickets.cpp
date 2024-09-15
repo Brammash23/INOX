a08e13c39197cccabc659d4fdaa401e93cd5d162
Brammash23 <brammash670@gamil.com>
2024-09-15 13:10:44 +0530

    Features:watch

---
+++ movietickets.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class audience
+ {
+ public:
+ 	
+ 	
+ }; 
+
+int main()
+{
+	int a;
+	cout<<"1.Reserve a Ticket:"<<endl;
+	cout<<"2.Exit:"<<endl;
+	cout<<"Enter your Choice:";
+	cin>>a;
+	for(int i=0;i<10;i++)
+	{
+		for(int j=0;j<10;j++)
+		{
+			cout<<" ___ ";
+		}
+		cout<<endl;
+	}
+    
+
+	
+
+
+	
+	return 0;
+}
